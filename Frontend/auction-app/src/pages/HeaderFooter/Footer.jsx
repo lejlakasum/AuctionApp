572b95284b44136afcbd7d4d17471931e313c016
Lejla Kasum <lkasum1@etf.unsa.ba>
2020-09-26 19:48:43 +0200

    Add empty header and footer

---
+++ Frontend/auction-app/src/pages/HeaderFooter/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../index.css"
+
+const Footer = props => {
+    return (
+        <div className="footer">
+            THIS IS FOOTER
+        </div>
+    )
+}
+
+export default Footer;
---
+++ Frontend/auction-app/src/pages/HeaderFooter/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../index.css"
+
+const Header = props => {
+    return (
+        <div className="header">
+            THIS IS HEADER
+        </div>
+    )
+}
+
+export default Header;
